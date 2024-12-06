d6191d68b37d9b87f44b5abcbc33ad931601a125
Isha Sangpal <143869326+binksBrew@users.noreply.github.com>
2024-12-06 18:25:15 +0530

    favicon update

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'FitNode',
+  location: 'us-central1'
+};
+
